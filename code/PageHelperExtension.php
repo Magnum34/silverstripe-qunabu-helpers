d2ff0e6693a876131cdeb748b4130a9efec94aa4
Mateusz Wojczal <mateusz@qunabu.com>
2017-07-11 12:36:33 +0200

    getJavaScriptLibFiles

--- code/PageHelperExtension.php
+++ code/PageHelperExtension.php
@@ -13,4 +13,17 @@ class PageHelperExtension extends DataExtension {
   public function isLive() {
     return Director::get_environment_type() == 'live';
   }
-} 
+  public function getJavaScriptLibFiles() {
+    $files = glob( BASE_PATH.'/'.$this->owner->ThemeDir().'/javascript/lib/*.js' );
+    usort($files, function($a, $b) {
+      return strcmp($b, $a);
+    });
+    $result = new ArrayList();
+    foreach($files as $file) {
+      $result->push(array(
+        'File'=>basename($file)
+      ));
+    }
+    return $result;
+  }
+}
