514890ff56718915215142b0cae61e12dfa0811c
Mateusz Wojczal <mateusz@qunabu.com>
2017-08-02 12:43:11 +0200

    ss env fix

--- help-files/_ss_environment.php
+++ help-files/_ss_environment.php
@@ -3,10 +3,10 @@
 define('SS_ENVIRONMENT_TYPE', 'dev');
 
 /* Database connection */
-define('SS_DATABASE_SERVER', 'localhost');
-define('SS_DATABASE_USERNAME', 'root');
-define('SS_DATABASE_NAME', 'stallan');
-define('SS_DATABASE_PASSWORD', 'root');
+define('SS_DATABASE_SERVER', '||SS_DATABASE_SERVER||');
+define('SS_DATABASE_USERNAME', '||SS_DATABASE_USERNAME||');
+define('SS_DATABASE_NAME', '||SS_DATABASE_NAME||');
+define('SS_DATABASE_PASSWORD', '||SS_DATABASE_NAME||');
 
 /* Configure a default username and password to access the CMS on all sites in this environment. */
 //define('SS_DEFAULT_ADMIN_USERNAME', 'username');
