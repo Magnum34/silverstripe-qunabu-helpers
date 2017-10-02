22e2b1b55fdbd0645ad57729c77ac836ae5afb41
Mateusz Qunabu <mateusz@qunabu.com>
2017-10-02 10:26:03 +0200

    Update build.sh

--- help-files/build.sh
+++ help-files/build.sh
@@ -1,9 +1,25 @@
 #!/usr/bin/env bash
 cd /home/qunabu/webapps/PROJECTNAME
+
+# remove all untracked files
+git clear -f
+
+# pull latest repos
 git pull
+
+# fetch all branches
 git fetch --all
+
+# checkoutr branch develop 
 git checkout --force "develop"
+
+#copy live settings to _config.pgp
 cp /home/qunabu/webapps/PROJECTNAME/mysite/live_config.php /home/qunabu/webapps/PROJECTNAME/mysite/_config.php
+
+#remove composer lock 
 rm composer.lock
-php56 /home/qunabu/bin/composer.phar update
+# run composer non-interactive mode
+php56 /home/qunabu/bin/composer.phar update -n 
+
+#build manifest and clear cache
 php56 /home/qunabu/webapps/PROJECTNAME/framework/cli-script.php dev/build "?flush=1"
